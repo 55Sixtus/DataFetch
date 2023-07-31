541284d9ad5d47d957a867ceaf42bfd089c3da20
Ezirim Sixtus chima <129880939+55Sixtus@users.noreply.github.com>
2023-07-31 14:18:39 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+
+
+<template>
+  <main>
+    <h1>search movie</h1>
+    <div class="input-btn">
+      <input type="text" v-model="search">
+      <button @click="getPosts">Fetch</button>
+    </div>
+    <div>
+      <ul>
+        <li :v-for="post in post">{{ post }}</li>
+      </ul>
+    </div>
+
+  </main>
+
+</template>
+
+ <script>
+export default{
+  data(){
+    return{
+      post: [],
+      search: ''
+    }
+  },
+  methods: {
+    getPosts(){
+      fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ea8b573d`)
+        .then(Response => Response.json())
+        .then(data => this.post = data)
+    }
+  }
+}
+
+</script>
+<style  scoped>
+main{
+  background-color: aqua;
+}
+
+</style>
+
+
